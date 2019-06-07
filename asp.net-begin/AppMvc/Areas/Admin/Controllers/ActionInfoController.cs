838c14993369941da49824e7f21da2c1e208044a
AllenHongjun <652971723@163.com>
2019-06-08 07:36:50 +0800

    视图模型层创建  viewmodel

--- asp.net-begin/AppMvc/Areas/Admin/Controllers/ActionInfoController.cs
+++ asp.net-begin/AppMvc/Areas/Admin/Controllers/ActionInfoController.cs
@@ -3,32 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC.IBLL;
+using MVC.Model;
 
 namespace AppMvc.Areas.Admin.Controllers
 {
     public class ActionInfoController : Controller
     {
+        private IActionInfoService ActionInfoService { get; set; }
         // GET: Admin/ActionInfo
         public ActionResult Index()
         {
+            
             return View();
+        }
+
+
+        /// <summary>
+        /// 分页获取权限列表数据
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetActionList()
+        {
+            #region 控制器中方法通用的步骤逻辑 
+            /*
+             * 验证数据的合法性
+             * 是否需要登陆
+             *
+                 *接受参数
+                 *判断参数的合法性
+                 *调用服务层的方法
+                 *返回的数据组装成我们需要的格式
+                 *客户端调用
+             *    缓存中数据读取
+             *
+                 *
+                 * lambda表单是的参数类型 泛型委托
+                 * 实体类型 实体类型的集合
+                 */ 
+            #endregion
+
+            int pageIndex = Request["page"] != null ? Convert.ToInt32(Request["page"]) : 1;
+            int pageSize = Request["rows"] != null ? Convert.ToInt32(Request["rows"]) : 10;
+            int total = 0;
+            var pageList = ActionInfoService.LoadPageEntities(pageIndex, pageSize, out total, x => x.ID > 1, x => x.ID, true)
+                .Select( x => new
+                {
+                    x.ID,
+                    x.ActionInfoName,
+                    x.IsMenu,
+                    x.SubTime,
+                    x.Url
+                }).ToList();
+            return Json(new {rows = pageList, total = total},JsonRequestBehavior.AllowGet);
         }
 
         // GET: Admin/ActionInfo/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var action = ActionInfoService.LoadEntities(x => x.ID == id).FirstOrDefault();
+            return Json(new {code = 1, data = action},JsonRequestBehavior.AllowGet);
         }
 
-        // GET: Admin/ActionInfo/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
 
         // POST: Admin/ActionInfo/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(ActionInfo actionInfo)
         {
             try
             {
@@ -42,11 +82,6 @@ namespace AppMvc.Areas.Admin.Controllers
             }
         }
 
-        // GET: Admin/ActionInfo/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
 
         // POST: Admin/ActionInfo/Edit/5
         [HttpPost]
@@ -62,12 +97,6 @@ namespace AppMvc.Areas.Admin.Controllers
             {
                 return View();
             }
-        }
-
-        // GET: Admin/ActionInfo/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
         }
 
         // POST: Admin/ActionInfo/Delete/5
---
+++ asp.net-begin/AppMvc/Areas/Admin/Models/ActionInfoViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppMvc.Areas.Admin.Models
+{
+    public class ActionInfoViewModel
+    {
+    }
+}
