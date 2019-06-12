812ffb3d4d4685c501898b04bff0725c686cb6d7
洪俊裕 <652971723@qq.com>
2019-06-12 14:24:10 +0800

    BookService 使用数据 webAPi 项目玩耍

---
+++ asp.net-webapi/day-2/BookService/BookService/Models/Author.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookService.Models
+{   
+    /// <summary>
+    /// 作者
+    /// </summary>
+    public class Author
+    {
+
+        /// <summary>
+        /// 作者ID
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 作者的名字
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ asp.net-webapi/day-2/BookService/BookService/Models/Book.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookService.Models
+{   
+    /// <summary>
+    /// 书籍
+    /// </summary>
+    public class Book
+    {   
+        /// <summary>
+        /// 书籍ID
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 书的标题
+        /// </summary>
+        [Required]
+        public string Title { get; set; }
+        /// <summary>
+        /// 出版时间
+        /// </summary>
+        public int Year { get; set; }
+
+        /// <summary>
+        /// 价格
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// 流派
+        /// </summary>
+        public string Genre { get; set; }
+
+        // Foreign Key
+        public int AuthorId { get; set; }
+
+        /// <summary>
+        /// 导航熟悉 不是一定要加个标记的
+        /// </summary>
+        // Navigation property
+        public Author Author { get; set; }
+    }
+}
