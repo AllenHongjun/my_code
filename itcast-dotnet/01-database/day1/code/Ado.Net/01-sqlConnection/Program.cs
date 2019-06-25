da3b941db1f65440bcf67e59cccc320430ae00df
AllenHongjun <652971723@163.com>
2019-06-26 07:44:35 +0800

    sql day7 ado.net 几个重要的对象 代码联系。。按照步骤 一步一步敲下来。

---
+++ itcast-dotnet/01-database/day1/code/Ado.Net/01-sqlConnection/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_sqlConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 就建立一个项目让后抄起来 。找博客个。。数据库也是可以一样饿的
+            //string strConn = "server=212.64.105.97;database=Hotel;uid=sa;pwd=I^OlRIE5deuaIS";
+            // 写一个软件 可以让用户点点就可以很方便的操作数据库。。  sql 执行计划如何看 如何看的懂  分析一下数据库执行的计划。。
+            // 如何看懂数据库执行的计划。。
+            // ado.net 微软一组操作 数据库的 类库。
+
+            /*
+             *SqlConnection sql连接的分装。  SqlCommand sql命令的分装  SqlPrivder
+             *
+             *
+             * DataSet DataTable    
+             *
+             *
+             */
+            string strConn = "server=.;database=Hotel;uid=sa;pwd=a123456";
+
+            #region MyRegion
+
+
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                //创建一个Sql命令对象  注意一下 重点掌握 sqlCommand 的几个重要属性和方法
+                //想敲代码 就在建立一个敲代码的地方。。建立一个解决方法的什么的
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    //给命令对象指定 连接对象。
+                    cmd.Connection = conn;
+
+                    conn.Open(); //一定要在执行命令之前打开就可以了。
+
+                    //此属性放我们的sql脚本
+                    cmd.CommandText = "insert into DboUserInfo(Name,DelFlag,Demo,cons)values('4444448888',0,'sss',0)";
+
+                    cmd.ExecuteNonQuery(); //执行一个非查询sql语句，返回受影响的行数。 
+
+                    //cmd.CommandText = "sel";
+                    //cmd.ExecuteNonQuery()。。。。
+                }
+            }
+
+
+            #endregion
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ itcast-dotnet/01-database/day1/code/Ado.Net/02-ConnectionPool/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_ConnectionPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *https://www.cnblogs.com/liuhaorain/archive/2012/02/19/2353110.html
+             *https://cloud.tencent.com/developer/article/1082887
+             *https://www.cnblogs.com/1zhk/articles/5075700.html
+             *
+             * ctrl + K ctrl + b sublime 切换左侧菜单快捷键
+             */
+            string connStrPool =
+                "Data Source=.;Initial Catalog=Hotel;User ID=sa;Password=a123456;Pooling=true;Min Pool Size=4";
+            string connStr = "Data Source=.;Initial Catalog= Hotel;User ID =sa; Password=a123456;Pooling=false";
+            int i = 0;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            while (i < 10000)
+            {   
+                //循环一次 创建一个连接对象 打开一次新的链接。模拟高并发的情况
+                using (SqlConnection conn = new SqlConnection(connStrPool))
+                {
+                    conn.Open();
+                }
+
+                i++;
+            }
+            sw.Stop();
+            //获取毫秒数量
+            Console.WriteLine(sw.Elapsed.Milliseconds);
+            sw.Reset();
+            sw.Restart();
+
+            i = 0;
+            while (i < 10000)
+            {
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    conn.Open();
+                }
+
+                i++;
+            }
+            sw.Stop();
+
+            // 明明都等了 5-6秒钟 他考试 这个时间  只有1秒钟不到。这个也是奇怪了。
+            // 
+            Console.WriteLine(sw.Elapsed.Milliseconds);
+            Console.ReadKey();
+
+        }
+    }
+}
